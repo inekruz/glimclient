32a5e37bc2505593c74e12a2f5c7ba0719e20aa4
inekruz <nekruz15ik@gmail.com>
2024-12-14 03:42:23 +0500

    add

---
+++ src/routes/Basket.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Routes.css';
+
+
+function Basket() {
+   return (
+      <div className='basket'>
+         basket
+      </div>
+   );
+}
+
+export default Basket;
