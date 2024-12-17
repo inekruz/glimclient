7e4df070931a5af9550a0143dc2b0ecf73f1336b
inekruz <nekruz15ik@gmail.com>
2024-12-17 14:36:58 +0500

    add profile data

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
     setUsername(null);
     setRole(null);
   };
-
+  
   if (!token) {
     return <Auth setToken={setToken} setUsername={setUsername} setRole={setRole} />;
   } else {
@@ -93,7 +93,7 @@ function App() {
               </li>
             </Link>
 
-            {role === 'Продавец' && (
+            {role === 1 && (
               <Link to='/addproduct' className={`nav_menu_list_item ${location.pathname === '/addproduct' ? 'active' : ''}`}>
                 <li className='nav_menu_list_item_container'>
                   <div className='menu_icon_container'>
