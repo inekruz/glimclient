7dfc6d9022a8769ad24f609df89521e30e3bfb88
inekruz <nekruz15ik@gmail.com>
2024-12-17 15:38:12 +0500

    add profile data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Link, Route, Routes, useLocation } from 'react-router-dom';
+
 import Auth from './components/Auth';
 import Main from './routes/Main';
 import Basket from './routes/Basket';
@@ -25,7 +26,7 @@ function App() {
   const location = useLocation();
   const [locationPopup, setLocationPopup] = useState(false);
   const [address, setAddress] = useState('');
-  const [searchQuery, setSearchQuery] = useState(''); // Новое состояние для поискового запроса
+  const [searchQuery, setSearchQuery] = useState('');
 
   const handleLogout = () => {
     localStorage.removeItem('token');
@@ -42,6 +43,7 @@ function App() {
     const fetchAddress = async () => {
       if (username) {
         try {
+          console.log("adrrsss:", username);
           const response = await fetch('https://api.glimshop.ru/getAddress', {
             method: 'POST',
             headers: {
@@ -57,6 +59,7 @@ function App() {
           const data = await response.json();
           if (data.length > 0) {
             setAddress(data[0].address);
+            console.log("add", data[0].address);
           } else {
             setAddress('Адрес не найден');
           }
@@ -73,6 +76,7 @@ function App() {
   if (!token) {
     return <Auth setToken={setToken} setUsername={setUsername} setRole={setRole} />;
   } else {
+    console.log("Получен роль: ", roleText);
     return (
       <div className="App">
         <header>
@@ -86,7 +90,7 @@ function App() {
             <input 
               placeholder='Поиск' 
               value={searchQuery} 
-              onChange={(e) => setSearchQuery(e.target.value)} // Обработчик изменения
+              onChange={(e) => setSearchQuery(e.target.value)}
             />
 
             <div>
@@ -101,6 +105,48 @@ function App() {
             </div>
           </div>
         </header>
+
+        <div className='nav_menu'>
+          <ul className='nav_menu_list'>
+            <Link to='/' className={`nav_menu_list_item ${location.pathname === '/' ? 'active' : ''}`}>
+              <li className='nav_menu_list_item_container'>
+                <div className='menu_icon_container'>
+                  <img alt='Иконка меню' src={MenuMain} className='menu_icon' />
+                </div>
+                <p>Главная</p>
+              </li>
+            </Link>
+
+            <Link to='/favorite' className={`nav_menu_list_item ${location.pathname === '/favorite' ? 'active' : ''}`}>
+              <li className='nav_menu_list_item_container'>
+ <div className='menu_icon_container'>
+                  <img alt='Иконка меню' src={MenuLike} className='menu_icon' />
+                </div>
+                <p>Отложенное</p>
+              </li>
+            </Link>
+
+            <Link to='/delivery' className={`nav_menu_list_item ${location.pathname === '/delivery' ? 'active' : ''}`}>
+              <li className='nav_menu_list_item_container'>
+                <div className='menu_icon_container'>
+                  <img alt='Иконка меню' src={MenuDelivery} className='menu_icon' />
+                </div>
+                <p>Доставки</p>
+              </li>
+            </Link>
+
+            {roleText === 'Продавец' && (
+              <Link to='/addproduct' className={`nav_menu_list_item ${location.pathname === '/addproduct' ? 'active' : ''}`}>
+                <li className='nav_menu_list_item_container'>
+                  <div className='menu_icon_container'>
+                    <img alt='Иконка меню' src={MenuAdd} className='menu_icon' />
+                  </div>
+                  <p>Добавить товар</p>
+                </li>
+              </Link>
+            )}
+          </ul>
+        </div>
 
         <div className='content'>
           <div className={`location_popup ${locationPopup ? 'active' : ''}`}>
