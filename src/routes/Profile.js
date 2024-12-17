08480c9ca0796ac2577d793a43911414732d17cc
inekruz <nekruz15ik@gmail.com>
2024-12-17 13:06:57 +0500

    add profile data

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -12,7 +12,7 @@ function Profile() {
    useEffect(() => {
       const fetchUserData = async () => {
          try {
-            const response = await fetch('https://api.glimshop.ru/getUser ', {
+            const response = await fetch('https://api.glimshop.ru/getUser', {
                method: 'POST',
                headers: {
                   'Content-Type': 'application/json',
